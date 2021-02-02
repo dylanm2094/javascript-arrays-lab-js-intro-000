b65f23cb6d1a6cdb48f12013e6d1891ae53d5885
Dylan Mann <dylanm2094@gmail.com>
2021-02-02 20:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   return kittens.shift()
 }
+
+function appendKitten(name) {
+  var new_kittens = [name,...kittens]
+  return new_kittens
+}
